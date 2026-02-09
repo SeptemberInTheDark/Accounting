b0890cb5295ce49f2f0455e43a4d297bad97c8e5
Андрей Соловьев <aa.solovev2@stalmail.org>
2026-02-09 10:30:12 +0300

    убрал редис, добавил init

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 from application.salary import calculate_salary
 from application.db.people import get_employees
 from datetime import datetime
-import redis
-
-r = redis.Redis(host='localhost', port=6379, db=0)
-r.set('foo', 'bar')
-print(r.get('foo'))
 
 
 if __name__ == '__main__':
